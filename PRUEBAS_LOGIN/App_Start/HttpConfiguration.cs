e6c6645466ee70604b2badcdd1fd4e4cb86e31db
Ronald <ragcorp.g@outlook.com>
2023-06-18 23:53:26 -0600

    servidor

---
+++ PRUEBAS_LOGIN/App_Start/HttpConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TOY_YODA
+{
+    public class HttpConfiguration
+    {
+        public object Routes { get; internal set; }
+
+        internal void MapHttpAttributeRoutes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
